ac1739ec9041ba8d842b71c8660553e5b9cae11e
matrix452 <61765417+matrix452@users.noreply.github.com>
2022-01-10 08:59:38 -0500

    Update CDR.py

--- CDR.py
+++ CDR.py
@@ -8,7 +8,6 @@ VARS = []
 EXTS = [306, 318, 322, 382, 438, 488, 547, 573, 588, 722, 751, 775, 812, 816, 2239, 2254, 2255, 2256, 2259,2260, 2261, 2401, 2575]
 df = pd.read_csv(CDR, usecols={'globalCallID_callId','dateTimeOrigination','callingPartyNumber','origCause_location','origCause_value','origPrecedenceLevel','destLegIdentifier','destNodeId','destSpan','originalCalledPartyNumber','finalCalledPartyNumber','destCause_location','destCause_value','dateTimeConnect','dateTimeDisconnect','lastRedirectDn','origDeviceName','destDeviceName','origCallTerminationOnBehalfOf','destCallTerminationOnBehalfOf','origCalledPartyRedirectOnBehalfOf','lastRedirectRedirectOnBehalfOf','originalCalledPartyPattern','finalCalledPartyPattern','lastRedirectingPartyPattern','huntPilotPattern','duration'})
 output_file = "CDR.xlsx"
-i = 0
 for ext in EXTS:
     dn = str(ext)
     ext_df = df.loc[(df['originalCalledPartyNumber'] == dn)]
